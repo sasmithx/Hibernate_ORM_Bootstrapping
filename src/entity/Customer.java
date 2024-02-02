888eb0a4305760b894361a4e3f5617a2e436558b
sasmithx <sasmithgit@gmail.com>
2024-02-02 16:29:16 +0530

    Customer Encapsulated

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,5 +1,49 @@
 package entity;
 
 public class Customer {
+    private int id;
+    private String name;
+    private String address;
+    private double salary;
 
+    public Customer() {}
+
+    public Customer(int id, String name, String address, double salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
