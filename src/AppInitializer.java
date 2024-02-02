55ddc97814bb93f7912a4981c10f6fda5d874d59
sasmithx <sasmithgit@gmail.com>
2024-02-02 16:47:58 +0530

    AppInitializer Customer Object set Data

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,20 @@
+import config.SessionFactoryConfig;
+import entity.Customer;
+import org.hibernate.Session;
+
 public class AppInitializer {
     public static void main(String[] args) {
-        System.out.println("Hello Sas!");
+
+        Session session = SessionFactoryConfig
+                .getInstance()
+                .getSession();
+
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Saman");
+        customer.setAddress("Galle");
+        customer.setSalary(25000);
+
+
     }
 }
