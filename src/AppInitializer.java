31b7e51f8ddceaf9dc8f66b58400a3bd476be2dc
sasmithx <sasmithgit@gmail.com>
2024-02-02 17:04:14 +0530

    AppInitializer session close

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,6 +1,7 @@
 import config.SessionFactoryConfig;
 import entity.Customer;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -9,13 +10,18 @@ public class AppInitializer {
                 .getInstance()
                 .getSession();
 
+        Transaction transaction = session.beginTransaction();
         Customer customer = new Customer();
         customer.setId(1);
-        customer.setName("Saman");
+        customer.setName("Kamal");
         customer.setAddress("Galle");
-        customer.setSalary(25000);
+        customer.setSalary(75000);
 
         session.save(customer);
+
+        transaction.commit();
+
+        session.close();
 
     }
 }
