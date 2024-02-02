c00618b4d63904bcddc5066e49458e1182304b6b
sasmithx <sasmithgit@gmail.com>
2024-02-02 15:56:50 +0530

    getInstance created

--- src/config/SessionFactoryConfig.java
+++ src/config/SessionFactoryConfig.java
@@ -3,5 +3,10 @@ package config;
 public class SessionFactoryConfig {
 
     private static SessionFactoryConfig factoryConfig;
+    private SessionFactoryConfig() {}
+
+    public static SessionFactoryConfig getInstance(){
+
+    }
 
 }
