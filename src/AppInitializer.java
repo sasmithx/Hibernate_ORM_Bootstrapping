ffea8d182e51213589d8eaada245a614f2335728
sasa-x <sasmithgit@gmail.com>
2024-02-13 18:20:48 +0530

    Fixed problems

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,77 +1,94 @@
 import config.SessionFactoryConfig;
+import embedded.MobileNumber;
+import embedded.NameIdentifier;
 import entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppInitializer {
     public static void main(String[] args) {
 
-        //1.save
-        Session session = SessionFactoryConfig
-                .getInstance()
-                .getSession();
+        //1.Save
+        System.out.println("------SAVE------");
+        Session session = SessionFactoryConfig.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
 
         Customer customer = getCustomer();
 
         session.save(customer);
-
         transaction.commit();
-
         session.close();
-        System.out.println("-----SAVE-----");
-        //2. Get
-        System.out.println("-----GET-----");
-        Session getSession = SessionFactoryConfig
-                .getInstance()
-                .getSession();
+        System.out.println("------SAVE------");
 
-        Customer existingCustomer =
-                getSession
-                .get(Customer.class, 2);
+        /*//2.Get
+        System.out.println("------GET------");
+        Session getsSession = SessionFactoryConfig.getInstance().getSession();
+        Customer existingCustomer = getsSession.get(Customer.class,1);
+        System.out.println("Existing Customer: " + existingCustomer);
+        getsSession.close();
+        System.out.println("------GET------");
 
-        System.out.println("Existing Customer :" + existingCustomer);
-
-        getSession.close();
-
-
-        //3. Update
-        System.out.println("-----UPDATE-----");
-        Session updateSession = SessionFactoryConfig
-                .getInstance()
-                .getSession();
+        //3.update
+        System.out.println("------UPDATE------");
+        Session updateSession = SessionFactoryConfig.getInstance().getSession();
+        Customer existingCus = updateSession.get(Customer.class, 1);
 
         Transaction updateTransaction = updateSession.beginTransaction();
-
-        Customer existingCus = updateSession.get(Customer.class, 2);
-
         existingCus.setAddress("Matara");
         updateSession.update(existingCus);
         updateTransaction.commit();
         updateSession.close();
+        System.out.println("------UPDATE------");
 
-
-        //4. delete
-        System.out.println("-----DELETE-----");
-        Session deleteSession = SessionFactoryConfig
-                .getInstance()
-                .getSession();
+        //4.delete
+        System.out.println("------DELETE------");
+        Session deleteSession = SessionFactoryConfig.getInstance().getSession();
+        Customer existingCusDelete = deleteSession.get(Customer.class, 1);
 
         Transaction deleteTransaction = deleteSession.beginTransaction();
-
-        Customer existingCustomerDelete = deleteSession.get(Customer.class, 2);
-        deleteSession.delete(existingCustomerDelete);
+        deleteSession.delete(existingCusDelete);
         deleteTransaction.commit();
         deleteSession.close();
+        System.out.println("------DELETE------");*/
     }
 
     private static Customer getCustomer() {
         Customer customer = new Customer();
         customer.setId(2);
-        customer.setName("Kamal");
-        customer.setAddress("Galle");
-        customer.setSalary(75000);
+        customer.setName(getNameIdentifier());
+        //
+        MobileNumber homeMobile = new MobileNumber();
+        homeMobile.setType("HOME");
+        homeMobile.setMobileNo("0411235997");
+
+        MobileNumber mobileNo = new MobileNumber();
+        mobileNo.setType("MOBILE");
+        mobileNo.setMobileNo("0702345768");
+
+        MobileNumber No = new MobileNumber();
+        mobileNo.setType("MOBILE_02");
+        mobileNo.setMobileNo("070234998");
+
+        List<MobileNumber> mobileNumbers = new ArrayList<>();
+        mobileNumbers.add(homeMobile);
+        mobileNumbers.add(mobileNo);
+        mobileNumbers.add(No);
+        //
+        customer.setMobileNumbers(mobileNumbers);
+        customer.setAddress("Rathnapura");
+        customer.setSalary(65000.00);
         return customer;
+    }
+
+    private static NameIdentifier getNameIdentifier() {
+        NameIdentifier nameIdentifier = new NameIdentifier();
+        nameIdentifier.setFirstName("ASHEN");
+        nameIdentifier.setMiddleName("sumudu");
+        nameIdentifier.setLastName("ARAvinda");
+        return nameIdentifier;
     }
 }
--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,33 +1,64 @@
 package entity;
 
+import embedded.MobileNumber;
+import embedded.NameIdentifier;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
-//1. @Entity(name = "customer")
+/*
+@Entity(name = "customer")
+        - - - OR - - -      */
 @Entity
 @Table(name = "customer")
 public class Customer {
 
-    @Id //Tells Hibernate that this is the Primary Key of this Table
+    @Id // Says Hibernate that 'private int id' should be the primary key
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "customer_id")
     private int id;
     @Column(name = "customer_name")
-    private String name;
+    private NameIdentifier name;
     @Column(name = "customer_address")
     private String address;
+    @Transient
     @Column(name = "customer_salary")
     private double salary;
+    /*@Transient
+    @Column(name = "customer_mobileNo")
+    private int mobileNumber;*/
+
+    @ElementCollection
+    @CollectionTable(
+            name = "customer_mobile_NoS",
+            joinColumns = @JoinColumn(name = "customer_id")
+    )
+    private List<MobileNumber> mobileNumbers = new ArrayList<>(); //Initializing Array List is a best practice
+
+    @CreationTimestamp
+    private Timestamp createdDateTime;
 
     public Customer() {}
 
-    public Customer(int id, String name, String address, double salary) {
+    public Customer(int id, NameIdentifier name, String address, double salary, List<MobileNumber> mobileNumbers) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.salary = salary;
+        this.mobileNumbers = mobileNumbers;
+    }
+
+    public NameIdentifier getName() {
+        return name;
+    }
+
+    public void setName(NameIdentifier name) {
+        this.name = name;
     }
 
     public int getId() {
@@ -36,14 +67,6 @@ public class Customer {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAddress() {
@@ -60,5 +83,25 @@ public class Customer {
 
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public List<MobileNumber> getMobileNumbers() {
+        return mobileNumbers;
+    }
+
+    public void setMobileNumbers(List<MobileNumber> mobileNumbers) {
+        this.mobileNumbers = mobileNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name=" + name +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                ", mobileNumbers=" + mobileNumbers +
+                ", createdDateTime=" + createdDateTime +
+                '}';
     }
 }
